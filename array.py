03c83557704e56b8ad807250b5344ac246aa1467
Knights24 <vivekvishwakarma21@outlook.com>
2026-04-27 18:26:42 +0530

    ops

--- array.py
+++ array.py
@@ -2,11 +2,12 @@ from array import *
 
 var = array('i',[1,2,3,4,5,6])
 
-for i in range(0,len(var)):
-    print(var[i])
+print(var)
+var.append(50)
+var.insert(2,10)
+var[3] = 100
 
-print('\n')
+copyArray = array(var.typecode, (x for x in var))  #first loop took element put in x then x from starting is storing all elements in list
 
-var.reverse()
-for x in var:
+for x in copyArray:
     print(x)
