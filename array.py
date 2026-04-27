cad11df474dd0ef63a27665472e1767cca7d5c75
Knights24 <vivekvishwakarma21@outlook.com>
2026-04-27 18:34:47 +0530

    slicing

--- array.py
+++ array.py
@@ -2,14 +2,9 @@ from array import *
 
 var = array('i',[1,2,3,4,5,6])
 
-print(var)
-var.append(50)
-var.insert(2,10)
-var[3] = 100
+# print(var)
 
-copyArray = array(var.typecode, (x for x in var))  #first loop took element put in x then x from starting is storing all elements in list
-multipleArray = array(copyArray.typecode, (x*3 for x in copyArray)) 
-
-for x in multipleArray:
-    print(x)
-
+var2 = var[1:5]
+print(var2)
+var3 = var[:3]
+print(var3)
